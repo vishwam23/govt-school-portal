8e8c23ae2c11757af8d358a06d2a7d11568d6787
Vishwam <138647459+vishwam23@users.noreply.github.com>
2026-03-22 13:17:10 +0530

    Create scrupt.js

---
+++ scrupt.js
@@ -0,0 +1,36 @@
+let students = [];
+
+fetch('students.json')
+    .then(res => res.json())
+    .then(data => {
+        students = data;
+        displayData(students);
+    });
+
+function displayData(data) {
+    const tableBody = document.querySelector("#studentTable tbody");
+    tableBody.innerHTML = "";
+
+    data.forEach(student => {
+        let row = `
+            <tr>
+                <td>${student.firstName}</td>
+                <td>${student.lastName}</td>
+                <td>${student.class}</td>
+                <td>${student.rollNo}</td>
+            </tr>
+        `;
+        tableBody.innerHTML += row;
+    });
+}
+
+function filterData() {
+    let search = document.getElementById("search").value.toLowerCase();
+
+    let filtered = students.filter(s =>
+        s.firstName.toLowerCase().includes(search) ||
+        s.lastName.toLowerCase().includes(search)
+    );
+
+    displayData(filtered);
+}
